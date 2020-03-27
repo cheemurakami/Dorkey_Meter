c0b6da028de1f836c80a64d4aa337397e389bb02
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-26 21:23:39 -0700

    Add function using if statements

---
+++ js/styles.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#questions").submit(function(event){
+    event.preventDefault();
+    // console.log($('input:checkbox:checked'))
+    var checkedNum = $('input:checkbox:checked').length
+    var result1 = "You are a professional nerd!"
+    var result2 = "There is quite a chance that you would be a nerd, practice more."
+    var result3 = "You must be regretting what you submitted just now."
+    // console.log(checkedNum);    
+    if (checkedNum > 7){
+      $('#result').show();
+      $('p').text(result1);
+    } else if (checkedNum <= 7 && checkedNum >= 4){
+      $('#result').show();
+      $('p').text(result2);
+    } else {
+      $('#result').show();
+      $('p').text(result3);
+    }
+
+   });
+});
